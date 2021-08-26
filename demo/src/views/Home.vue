6a57b91fba7403159cbebce45fe2bd2c96e0f22c
panboren <384953161@qq.com>
2021-08-27 00:36:13 +0800

    demo

---
+++ demo/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">index</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  width: 100px;
+  height: 100px;
+  font-size: 20px;
+  background: #d5b4b4;
+}
+</style>
